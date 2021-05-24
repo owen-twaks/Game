7f4ee8fe1266d5983673a3d6811323b0270fafd5
owen-twaks <matsalani@yahoo.com>
2021-05-24 20:48:18 +0200

    my-app-game

---
+++ src/data.js
@@ -0,0 +1,51 @@
+// all imagess store them on a Card Array//
+import IMG1 from "./images/car1.jpg";
+import IMG2 from "./images/car2.jpg";
+import IMG3 from "./images/car3.jpg";
+import IMG4 from "./images/car4.jpg";
+import IMG5 from "./images/car5.jpg";
+import IMG6 from "./images/car6.jpg";
+import IMG7 from "./images/car7.jpg";
+import IMG8 from "./images/car8.jpg";
+import IMG9 from "./images/car9.jpg";
+
+const uniqueCardsArray = [
+  {
+    type: "car3",
+    images: IMG1,
+  },
+  {
+    type: "car5",
+    images: IMG2,
+  },
+  {
+    type: "car6",
+    images: IMG3,
+  },
+  {
+    type: "car7",
+    images: IMG4,
+  },
+  {
+    type: "car8",
+    images: IMG5,
+  },
+  {
+    type: "car2",
+    images: IMG6,
+  },
+  {
+    type: "car4",
+    images: IMG7,
+  },
+  {
+    type: "car9",
+    images: IMG8,
+  },
+  {
+    type: "car1",
+    images: IMG9,
+  },
+];
+
+export default uniqueCardsArray;
